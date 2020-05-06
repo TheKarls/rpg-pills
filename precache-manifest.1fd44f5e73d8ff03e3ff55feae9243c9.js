5957b7d93d842de3fff79b26a1d52e7967ae5448
Karl <carlo.bussi@outlook.it>
2020-05-06 21:31:13 +0200

    Updates

---
+++ precache-manifest.1fd44f5e73d8ff03e3ff55feae9243c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d26eeaf8619fd4a412cebe5fb44aa95",
+    "url": "/rpg-pills/index.html"
+  },
+  {
+    "revision": "bd3dbc33029f6ec74a7f",
+    "url": "/rpg-pills/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "41119ea27e6540d5b992",
+    "url": "/rpg-pills/static/js/2.697d7adc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rpg-pills/static/js/2.697d7adc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd3dbc33029f6ec74a7f",
+    "url": "/rpg-pills/static/js/main.c645b768.chunk.js"
+  },
+  {
+    "revision": "5c3c5d9c65db09d6ab45",
+    "url": "/rpg-pills/static/js/runtime-main.f13ca426.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rpg-pills/static/media/logo.5d5d9eef.svg"
+  }
+]);
