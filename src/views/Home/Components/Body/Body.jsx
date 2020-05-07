5882f98a86e2a9a8ad00813ff7d2f662fa7ed795
Karl <carlo.bussi@outlook.it>
2020-05-07 02:26:44 +0200

    Coming Soon... added

--- src/views/Home/Components/Body/Body.jsx
+++ src/views/Home/Components/Body/Body.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 
 export default function Body(props){
     return(
-        <></>
+        <>
+
+            <div className="coming">
+                <img src={require("../../../../img/icon.svg")} className="logo" alt="Logo"/>
+                <h1>Coming Soon...</h1>
+            </div>
+        
+        </>
     )
 }
